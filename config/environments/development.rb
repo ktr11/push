2a3961f8659af7839252e3cfd4411836511bfc16
ktr11 <kenmcz315@gmail.com>
2021-04-21 09:27:09 +0900

    #7 表示しようとしたら接続を許可しないエラーになったので

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,4 +58,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # dockerのgatewayからの接続を許可
+  config.web_console.whitelisted_ips = '172.22.0.1'
 end
